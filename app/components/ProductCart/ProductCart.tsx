47235dd22bd6ad60e2f3d28b9af63b02eaf36a0d
leonel ka <waboleonel@gmail.com>
2024-05-23 22:20:04 +0100

    Style with tailwindcss

--- app/components/ProductCart/ProductCart.tsx
+++ app/components/ProductCart/ProductCart.tsx
@@ -1,9 +1,8 @@
 import AddToCart from "../AddToCart/AddToCart";
-import styles from "./ProductCart.module.css";
 
 const ProductCart = () => {
   return (
-    <div className={styles.cart}>
+    <div className="p-4 text-xl text-sky-500 bg-lime-100 font-black hover:text-sky-600 hover:text-2xl">
       <AddToCart />
     </div>
   );
