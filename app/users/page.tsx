69184719957c4b8eb11a4f41dddaf1ee5554d46f
leonel ka <waboleonel@gmail.com>
2024-05-25 10:34:15 +0100

    Loading page

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { Suspense } from "react";
 import UsersTable from "./UsersTable";
 
 interface Props {
@@ -9,7 +10,9 @@ const UsersPage = async ({ searchParams: { sort } }: Props) => {
   return (
     <>
       <Link href={"/users/new"}>Create</Link>
-      <UsersTable sortCriteria={sort} />
+      <Suspense fallback={<p>Loading...</p>}>
+        <UsersTable sortCriteria={sort} />
+      </Suspense>
     </>
   );
 };
